0820190a4e4254c8be73fc4589d112bab1543200
Dawid Schmidt <schmidtdawid@gmail.com>
2019-09-30 00:02:05 +0200

    datepicker

--- lib/widgets/create_new_todo.dart
+++ lib/widgets/create_new_todo.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class CreateNewTodo extends StatefulWidget {
   final Function addFunction;
@@ -11,16 +12,31 @@ class CreateNewTodo extends StatefulWidget {
 
 class _CreateNewTodoState extends State<CreateNewTodo> {
   final titleController = TextEditingController();
+  DateTime _selectedDate;
 
-  void addTodo() {
+  void _addTodo() {
     final enteredTitle = titleController.text;
 
-    if(enteredTitle.isEmpty){
+    if (enteredTitle.isEmpty) {
       return;
     }
 
     widget.addFunction(titleController.text, 1);
     Navigator.of(context).pop();
+  }
+
+  void _presentDatePicker() {
+    showDatePicker(
+            context: context,
+            initialDate: DateTime.now(),
+            lastDate: DateTime.now().subtract(Duration(days: -1000)),
+            firstDate: DateTime(1970))
+        .then((date) {
+          if(date == null){return;}
+          setState(() {
+            _selectedDate = date;
+          });
+        });
   }
 
   @override
@@ -35,14 +51,26 @@ class _CreateNewTodoState extends State<CreateNewTodo> {
               controller: titleController,
               autocorrect: true,
               decoration: InputDecoration(labelText: 'Co chcesz zrobić?'),
-              onSubmitted: (_) => addTodo(),
+              onSubmitted: (_) => _addTodo(),
             ),
-            FlatButton(
-              child: Text(
-                "Dodaj",
-                style: TextStyle(color: Colors.pink),
+            Container(
+              height: 70,
+              child: Row(
+                children: <Widget>[
+                  Expanded(child: Text( _selectedDate == null ? 'Nie wybrano daty' :  DateFormat.MMMMEEEEd().format(_selectedDate))),
+                  FlatButton(
+                    child: Text('Wybierz datę'),
+                    textColor: Theme.of(context).primaryColor,
+                    onPressed: _presentDatePicker,
+                  )
+                ],
               ),
-              onPressed: addTodo,
+            ),
+            RaisedButton(
+              child: Text("Dodaj"),
+              color: Theme.of(context).primaryColor,
+              textColor: Theme.of(context).buttonColor,
+              onPressed: _addTodo,
             )
           ],
         ),
